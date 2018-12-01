35870578fe33092a26add5bcd960794f2469641f
Cecilia <cema3961@colorado.edu>
2018-11-30 17:07:07 -0700

    Make seperate Dockerfile for GPU setup

--- docker_setup.sh
+++ docker_setup.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-# Install Tkinter
-# apt-get update --assume-yes
-# apt-get install python3-tk --assume-yes
-
 # Install any needed packages specified in requirements.txt
-pip install --trusted-host pypi.python.org -r requirements.txt
+if [ ${ENV} = "GPU" ]; then
+    pip install --trusted-host pypi.python.org -r requirements_gpu.txt
+else
+    pip install --trusted-host pypi.python.org -r requirements.txt
+fi
+
 python setup.py install
 
 # Compile cocoapi
